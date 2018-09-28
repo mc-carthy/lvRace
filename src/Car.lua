21072dab58e370080166be1c17fc7ea316b46645
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-09-28 21:14:12 +0100

    Add basic speed sensitive steering

--- src/Car.lua
+++ src/Car.lua
@@ -3,20 +3,20 @@ local Vector2 = require("src.utils.Vector2")
 local car = {}
 
 local update = function(self, dt)
+    local dRot = 0
     if love.keyboard.isDown("up") or love.keyboard.isDown("w") then
-        local dx, dy = Vector2.pointFromRotDist(math.deg(self.rot), self.moveSpeed * dt)
+        dx, dy = Vector2.pointFromRotDist(math.deg(self.rot), self.moveSpeed * dt)
+        dRot = (self.rotSpeed / (Vector2.magnitude(dx, dy) / self.moveSpeed))
         self.x = self.x + dx
         self.y = self.y + dy
     end
 
     if love.keyboard.isDown("left") or love.keyboard.isDown("a") then
-        local dRot = self.rotSpeed * dt
-        self.rot = self.rot + dRot
+        self.rot = self.rot + dRot * dt
     end
 
     if love.keyboard.isDown("right") or love.keyboard.isDown("d") then
-        local dRot = self.rotSpeed * dt
-        self.rot = self.rot - dRot
+        self.rot = self.rot - dRot * dt
     end
 end
 
@@ -31,8 +31,8 @@ car.create = function(spritePath, x, y, rot)
     inst.x = x
     inst.y = y
     inst.rot = rot or 0
-    inst.moveSpeed = 150
-    inst.rotSpeed = 1.5
+    inst.moveSpeed = 450
+    inst.rotSpeed = 0.05
 
     inst.update = update
     inst.draw = draw
